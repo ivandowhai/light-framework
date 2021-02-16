9ff0a24edce4c59366489389046f701e67c82c31
Ivan Dovhai <idovha@softserveinc.com>
2021-02-16 14:54:08 +0200

    Change paths in index.php and console

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/Autoloader.php';
+require_once __DIR__ . '/vendor/ivandowhai/light-framework/src/Autoloader.php';
 
 
 use Light\ {
